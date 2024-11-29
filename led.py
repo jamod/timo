785c35528f7148c02ad8f08024876c7c6aa23d84
jamod <segar.pascal@gmail.com>
2024-11-29 13:39:43 -0500

    commit-3 led.py with github signed

--- led.py
+++ led.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
+from time import sleep # Import the sleep function from the time modul
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
