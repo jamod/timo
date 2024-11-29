33b8952901cf78fe86e9594e1cb390092ed7904d
jamod <segar.pascal@gmail.com>
2024-11-29 13:28:48 -0500

    commit-2 led.py with github signed

--- led.py
+++ led.py
@@ -1,12 +1,18 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW) 
 
 while True: # Run forever
  GPIO.output(8, GPIO.HIGH) # Turn on
- sleep(1) # Sleep for 1 second
+ sleep(0.5) # Sleep for 1 second
  GPIO.output(8, GPIO.LOW) # Turn off
- sleep(1) # Sleep for one second
+ sleep(0.4) # Sleep for one second
+ 
+ GPIO.output(10, GPIO.HIGH) # Turn on
+ sleep(0.3) # Sleep for 1 second
+ GPIO.output(10, GPIO.LOW) # Turn off
+ sleep(0.2) # Sleep for one second
