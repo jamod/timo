dbc1e33fc1ee4d442191c31290211123be987114
jamod <segar.pascal@gmail.com>
2024-11-29 13:17:08 -0500

    commit led.py with github signed

--- led.py
+++ led.py
@@ -9,4 +9,4 @@ while True: # Run forever
  GPIO.output(8, GPIO.HIGH) # Turn on
  sleep(1) # Sleep for 1 second
  GPIO.output(8, GPIO.LOW) # Turn off
- sleep(1) # Sleep for 1 second
+ sleep(1) # Sleep for one second
